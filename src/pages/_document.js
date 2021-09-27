1970624cc84f9386f7d9dcb4889e0763311e93e5
Rajan Shrestha <rajansoftnep@gmail.com>
2021-09-27 10:07:40 +0545

    hompage

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,7 +15,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
 
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+          <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></Script>
         </body>
       </Html>
     );
